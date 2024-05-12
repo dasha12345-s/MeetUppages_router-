3ab8631fd9e0273370b6a6e2b436fe3e939c1849
Darya <shostak19@icloud.com>
2024-05-12 12:29:38 -0400

    Using Programmatic (Imperative) Navigation

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7541e2a6c947dbc5"; }
+/******/ 		__webpack_require__.h = function() { return "c3437ea91e6baf45"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
