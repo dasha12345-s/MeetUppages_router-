338684621b725d6af2c245215c95379a21c16e3a
Darya <shostak19@icloud.com>
2024-05-12 12:29:39 -0400

    More on Static Site Generation (SSG)

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f85542c80b54c54c"; }
+/******/ 		__webpack_require__.h = function() { return "2e276aa3258877c4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
