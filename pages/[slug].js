0abb7df0c0a5a2604dde6ad49568631740c023fa
Darya <shostak19@icloud.com>
2024-05-12 14:18:43 -0400

    Getting Meetup Details Data & Preparing Pages

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,51 +1,67 @@
+import { MongoClient, ObjectId } from 'mongodb';
+
 import MeetupDEteil from '../components/meetups/MeetupDetail'
 
-function MeetupDet(){
+function MeetupDet(props){
 
   return(
     <MeetupDEteil 
-    image='https://images.pexels.com/photos/533769/pexels-photo-533769.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-    title='First MeetUP'
-    address='Some Street 5, Some City'
-    descriptio='The meetup description'/>
+    image={props.meetupData.image}
+    title={props.meetupData.title}
+    address={props.meetupData.address}
+    description={props.meetupData.description}/>
   )
 }
 
 export async function getStaticPaths(){
+
+  const client = await MongoClient.connect('mongodb+srv://shostak19:This-199606@cluster0.ithzofj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+  const db = client.db();
+
+  const meetupsCollections = db.collection('MeetUPs');
+
+  const meetups = await meetupsCollections.find({}, {
+    _id: 1
+  }).toArray();
+
+  client.close();
+
   return{
     fallback: false,
-    paths:[
+    paths: meetups.map(meetup => (
       {
         params: {
-          slug: 'm1',
-        },
-      },
-      {
-        params: {
-          slug: 'm2',
+          slug: meetup._id.toString()
         }
       }
-    ]
-  }
+    )) }
 }
 
 export async function getStaticProps(context){
 
   const slug = context.params.slug;
 
-  console.log(slug)
+  const client = await MongoClient.connect('mongodb+srv://shostak19:This-199606@cluster0.ithzofj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+  const db = client.db();
+
+  const meetupsCollection = db.collection('MeetUPs');
+
+  const selectedMeetup = await meetupsCollection.findOne({
+    _id: new ObjectId(slug)
+  });
+  client.close();
+  
 
   return{
     props: {
       meetupData: {
-        image: 'https://images.pexels.com/photos/533769/pexels-photo-533769.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-        id: slug,
-        title: 'first meetup',
-        address: 'address',
-        description: 'First'
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        image: selectedMeetup.image,
+        description: selectedMeetup.description,
+      },
       }
     }
   }
-}
-
 export default MeetupDet;
