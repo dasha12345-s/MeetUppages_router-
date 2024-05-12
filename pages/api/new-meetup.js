c254709679aa93227bd930b9632297e5fd02184d
Darya <shostak19@icloud.com>
2024-05-12 12:29:41 -0400

    Working with MongoDB

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,6 +1,21 @@
-function handler(req, res){
+import { MongoClient } from "mongodb";
+
+async function handler(req, res){
   if(req.method === 'POST'){
     const data = req.body;
+
+    const {title, image, address, description} = data;
+
+    const client = await MongoClient.connect('mongodb+srv://shostak19:kMMXM6Oq0mny5GEW@cluster0.ithzofj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+    const db = client.db();
+  
+    const meetupsCollections = db.collection('MeetUPs');
+    const result = await meetupsCollections.insertOne(data);
+    console.log(result);
+
+    client.close();
+
+    res.status(201).jsonnn({message: 'Meetup inserted!'});
   }
 }
 
