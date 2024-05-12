501c9a30a591ad170d6933c09e5cfb1829c3f339
Darya <shostak19@icloud.com>
2024-05-12 12:29:40 -0400

    Introducing API Routes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e912b615b49e070b"; }
+/******/ 		__webpack_require__.h = function() { return "dbf2e645dacc68c7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ pages/api/new-meetup.js
@@ -0,0 +1,7 @@
+function handler(req, res){
+  if(req.method === 'POST'){
+    const data = req.body;
+  }
+}
+
+export default handler;
